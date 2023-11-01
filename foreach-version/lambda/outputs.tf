f4b5f36b128fd4abb6e4706eed05a9f4cdaa2e0b
ertan uygan <ertan@budaai.com>
2023-11-01 13:16:30 +0300

    change root

--- foreach-version/lambda/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "lambda_arns" {
-  description = "ARNs of the Lambda functions"
-  value       = { for conf in var.lambda_configurations : conf.function_name => aws_lambda_function.this[conf.function_name].arn }
-}
--- foreach-version/layers/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "layer_arns" {
-  description = "The ARNs of the Lambda layers."
-  value       = { for k, v in aws_lambda_layer_version.this : k => v.arn }
-}
