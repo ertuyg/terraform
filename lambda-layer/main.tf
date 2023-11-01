8a10b3034e04889ca3444a14e1fd22ed80cc51e0
ertan uygan <ertan@budaai.com>
2023-11-01 10:00:21 +0300

    layer added

--- lambda-layer/main.tf
+++ lambda-layer/main.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_layer_version" "this" {
   layer_name          = var.name
-  filename            = data.archive_file.layer_zip.this.output_path
-  source_code_hash    = data.archive_file.layer_zip.this.output_base64sha256
+  filename            = data.archive_file.this.output_path
+  source_code_hash    = data.archive_file.this.output_base64sha256
   description         = var.description
   compatible_runtimes = var.compatible_runtimes
 }
