699623d7230a7f3f584f7432c8d1f3f8311b8d2b
ertan uygan <ertan@budaai.com>
2024-02-08 15:01:15 +0300

    ddd

--- cloudfront/main.tf
+++ cloudfront/main.tf
@@ -26,10 +26,10 @@ data "aws_s3_bucket" "this" {
 #   }
 # }
 
-resource "aws_s3_bucket_policy" "s3_policy" {
-  bucket = data.aws_s3_bucket.this.id
-  policy = data.aws_iam_policy_document.s3_policy.json
-}
+# resource "aws_s3_bucket_policy" "s3_policy" {
+#   bucket = data.aws_s3_bucket.this.id
+#   policy = data.aws_iam_policy_document.s3_policy.json
+# }
 
 
 # resource "aws_s3_bucket" "this" {
