128c961787bf5d3a83ab66ad10db4375d16b084d
ertan uygan <ertan@budaai.com>
2024-02-12 11:04:33 +0300

    aaaa

--- cloudfront/main.tf
+++ cloudfront/main.tf
@@ -6,13 +6,15 @@ resource "aws_cloudfront_origin_access_control" "this" {
   signing_protocol                  = "sigv4"
 }
 
-# resource "aws_cloudfront_origin_access_identity" "this" {
-#   comment = "origin identity"
-# }
-
 data "aws_s3_bucket" "this" {
   bucket = var.bucket_name
 }
+
+# eğer s3 bucket oluşturulmamışsa
+# resource "aws_s3_bucket" "this" {
+#   bucket = var.bucket_name
+# }
+
 
 resource "aws_s3_bucket_policy" "cdn-oac-bucket-policy" {
   bucket = var.bucket_name
@@ -36,11 +38,12 @@ data "aws_iam_policy_document" "s3_bucket_policy" {
 }
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
+  #TODO: originleri variablelerden alacak bir yapıı kurmalı
   origin {
     domain_name              = data.aws_s3_bucket.this.bucket_regional_domain_name
     origin_access_control_id = aws_cloudfront_origin_access_control.this.id
     origin_id                = var.s3_origin_id
-    origin_path              = "/images"
+    #origin_path              = "/images" #bu path bucket içerisindeki path yani eğer bunu koyarsan bucket içindeki images klasörüne /images yazmadan erişebilirsin
   }
 
   enabled         = true
@@ -75,30 +78,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     max_ttl                = 86400
   }
 
-  # Cache behavior with precedence 0
-  #   ordered_cache_behavior {
-  #     path_pattern     = "/content/immutable/*"
-  #     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-  #     cached_methods   = ["GET", "HEAD", "OPTIONS"]
-  #     target_origin_id = var.s3_origin_id
 
-  #     forwarded_values {
-  #       query_string = false
-  #       headers      = ["Origin"]
-
-  #       cookies {
-  #         forward = "none"
-  #       }
-  #     }
-
-  #     min_ttl                = 0
-  #     default_ttl            = 86400
-  #     max_ttl                = 31536000
-  #     compress               = true
-  #     viewer_protocol_policy = "redirect-to-https"
-  #   }
-
-  # Cache behavior with precedence 1
+  #TODO: cache behaviorları variablelerden alacak bir yapıı kurmalı
   ordered_cache_behavior {
     path_pattern     = "/*"
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
