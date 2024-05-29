6962e5d9cca0bd37ec2e458cec02985efa8d251e
ertan uygan <ertan@budaai.com>
2024-05-29 10:15:39 +0300

    hash kontrol için lifecycle eklendi

--- lambda-layer/main.tf
+++ lambda-layer/main.tf
@@ -4,10 +4,17 @@ resource "aws_lambda_layer_version" "this" {
   source_code_hash    = data.archive_file.this.output_base64sha256
   description         = var.description
   compatible_runtimes = var.compatible_runtimes
+  lifecycle {
+    ignore_changes = [
+      filename,
+      source_code_hash,
+    ]
+  }
 }
 
 data "archive_file" "this" {
   type        = "zip"
   source_dir  = var.source_path
-  output_path = "s3_archives/layers/${var.name}.zip"
+  output_path = "${path.module}/s3_archives/layers/${var.name}.zip"
 }
+
