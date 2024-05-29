8fc147106f84912ef6d2ad7b8d7eeabe667689ef
ertan uygan <ertan@budaai.com>
2024-05-29 17:51:26 +0300

    aa

--- lambda-layer/main.tf
+++ lambda-layer/main.tf
@@ -4,12 +4,12 @@ resource "aws_lambda_layer_version" "this" {
   source_code_hash    = data.archive_file.this.output_base64sha256
   description         = var.description
   compatible_runtimes = var.compatible_runtimes
-  lifecycle {
-    ignore_changes = [
-      filename,
-      source_code_hash,
-    ]
-  }
+  # lifecycle {
+  #   ignore_changes = [
+  #     filename,
+  #     source_code_hash,
+  #   ]
+  # }
 }
 
 data "archive_file" "this" {
