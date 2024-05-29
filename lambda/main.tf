18d37ac7720df603fce1d16fd82621ae16f254ab
ertan uygan <ertan@budaai.com>
2024-05-29 10:16:10 +0300

    fix

--- lambda/main.tf
+++ lambda/main.tf
@@ -2,7 +2,7 @@ data "archive_file" "this" {
   type        = "zip"
   source_dir  = can(file(var.source_path)) ? null : var.source_path
   source_file = can(file(var.source_path)) ? var.source_path : null
-  output_path = "s3_archives/lambda/${var.function_name}.zip"
+  output_path = "${path.module}/s3_archives/lambda/${var.function_name}.zip"
 }
 
 resource "aws_lambda_function" "this" {
