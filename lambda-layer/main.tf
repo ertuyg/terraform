c847d259ed6a661e2c28c9e84e15858127623332
ertan uygan <ertan@budaai.com>
2025-04-30 17:10:09 +0300

    aaa

--- lambda-layer/main.tf
+++ lambda-layer/main.tf
@@ -23,13 +23,13 @@ data "archive_file" "this" {
   count       = var.use_existing_zip ? 0 : 1
   type        = "zip"
   source_dir  = var.source_path
-  output_path = "s3_archives/layers/${var.name}.zip"
+  output_path = "${path.module}/s3_archives/layers/${var.name}.zip"
 }
 
 resource "aws_lambda_layer_version" "this" {
   layer_name          = var.name
-  filename            = var.use_existing_zip ? "s3_archives/layers/${var.name}.zip" : data.archive_file.this[0].output_path
-  source_code_hash    = var.use_existing_zip ? filesha256("s3_archives/layers/${var.name}.zip") : data.archive_file.this[0].output_base64sha256
+  filename            = var.use_existing_zip ? "${path.module}/s3_archives/layers/${var.name}.zip" : data.archive_file.this[0].output_path
+  source_code_hash    = var.use_existing_zip ? filesha256("${path.module}/s3_archives/layers/${var.name}.zip") : data.archive_file.this[0].output_base64sha256
   description         = var.description
   compatible_runtimes = var.compatible_runtimes
 }
