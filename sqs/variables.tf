403c086ed7b5f0d7c853de2d92a3d19db9e472ce
ertan uygan <ertan@budaai.com>
2025-04-30 11:05:41 +0300

    aaa

--- sqs/variables.tf
+++ sqs/variables.tf
@@ -3,10 +3,7 @@ variable "queue_name" {
   type        = string
 }
 
-variable "lambda_function_name" {
-  description = "The name of the Lambda function to trigger"
-  type        = string
-}
+
 variable "lambda_function_arn" {
   description = "The ARN of the Lambda function to trigger"
   type        = string
